61574cee2e87d4df10aa0a71d6d5a94c3e1f48ad
Zhu Rengen <zhurungen@wecash.net>
2021-03-17 17:47:09 +0800

    update

--- api/controller/grafana.go
+++ api/controller/grafana.go
@@ -1,14 +1,25 @@
 package controller
 
 import (
+	"encoding/json"
 	"github.com/cloverzrg/wechat-work-message-push-go/grafana"
+	"github.com/cloverzrg/wechat-work-message-push-go/logger"
 	"github.com/cloverzrg/wechat-work-message-push-go/qyapi"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 )
 
 func GrafaneHandler(c *gin.Context) {
+
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	logger.Info("GrafaneHandler: ", string(bytes))
 	noti := grafana.Notification{}
-	err := c.BindJSON(&noti)
+	err = json.Unmarshal(bytes, &noti)
+	//err = c.BindJSON(&noti)
 	if err != nil {
 		c.String(500, err.Error())
 		return
