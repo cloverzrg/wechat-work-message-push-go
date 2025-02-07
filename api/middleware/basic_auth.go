799eb8ea692a9e8a651500212182f66eab81bdbe
Rungen Zhu <rungen.zhu@shopee.com>
2025-02-07 18:40:07 +0800

    fix(api): 添加基本认证时的错误处理
    - 在基本认证中增加了对用户配置的检查
    - 如果用户配置为空，则返回 500 错误并提示配置缺失
    - 优化了错误处理，提高了系统的健壮性

--- api/middleware/basic_auth.go
+++ api/middleware/basic_auth.go
@@ -1,11 +1,18 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/cloverzrg/wechat-work-message-push-go/config"
 	"github.com/gin-gonic/gin"
 )
 
 func BasicAuth() func(c *gin.Context) {
+	if config.Config.GrafanaWebhookUser == "" {
+		return func(c *gin.Context) {
+			_ = c.AbortWithError(500, fmt.Errorf("Grafana Webhook User not set in configuration. Please check your config file. "))
+			return
+		}
+	}
 	return gin.BasicAuth(gin.Accounts{
 		config.Config.GrafanaWebhookUser: config.Config.GrafanaWebhookPassword,
 	})
